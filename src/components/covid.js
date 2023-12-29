7ac424be843137e4b8468bdb8c7c28169512b9c5
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2023-12-28 21:50:15 -0500

    CSS updates

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,113 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './covid.css';
 
 const Covid = () => {
+  const [data, setData] = useState({});
 
-  const [data, setData] = useState([]);
-
-
-  //async function returns promise. To make sure the system waits for the response to come back, we use await.
   const getCovidData = async () => {
-
     try {
       const res = await fetch('https://data.covid19india.org/data.json');
       const actualData = await res.json();
       setData(actualData.statewise[0]);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
+  };
 
-
-  }
-
-  //[] make it runs only once when the page loads. 
   useEffect(() => {
     getCovidData();
-  }, [])
-
+  }, []);
 
   return (
     <>
-    <section>
-      <h1> ** LIVE **</h1>
-      <h2>Corona virus tracker</h2>
+      <section className='covid-container'>
+        <h1 className='header'>LIVE</h1>
+        <h2 className='sub-header'>Coronavirus Tracker</h2>
 
-      <ul className='top'>
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> OUR </span>
-                COUNTRY
-              </p>
-              <p className='card_total card_small'> INDIA</p>
+        <ul className='card-container'>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Our Country</p>
+              <p className='card-total card-small'>INDIA</p>
             </div>
-          </div>
-        </li>
+          </li>
 
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> TOTAL </span>
-                RECOVERED
-              </p>
-              <p className='card_total card_small'> {data.recovered}</p>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Total Recovered</p>
+              <p className='card-total card-small'>{data.recovered}</p>
             </div>
-          </div>
-        </li>
+          </li>
 
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> TOTAL </span>
-                CONFIRMED
-              </p>
-              <p className='card_total card_small'> {data.confirmed}</p>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Total Confirmed</p>
+              <p className='card-total card-small'>{data.confirmed}</p>
             </div>
-          </div>
-        </li>
+          </li>
 
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> TOTAL </span>
-                DEATHS
-              </p>
-              <p className='card_total card_small'> {data.deaths}</p>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Total Deaths</p>
+              <p className='card-total card-small'>{data.deaths}</p>
             </div>
-          </div>
-        </li>
+          </li>
 
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> TOTAL </span>
-                ACTIVE
-              </p>
-              <p className='card_total card_small'> {data.active}</p>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Total Active</p>
+              <p className='card-total card-small'>{data.active}</p>
             </div>
-          </div>
-        </li>
+          </li>
 
-        <li className='card'>
-          <div className='card_main'>
-            <div className='card_inner'>
-              <p className='card_name'>
-                <span> LAST </span>
-                UPDATED
-              </p>
-              <p className='card_total card_small'> {data.lastupdatedtime}</p>
+          <li className='card'>
+            <div className='card-inner'>
+              <p className='card-name'>Last Updated</p>
+              <p className='card-total card-small'>{data.lastupdatedtime}</p>
             </div>
-          </div>
-        </li>
-      </ul>
-  </section>
+          </li>
+        </ul>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Covid
+export default Covid;
